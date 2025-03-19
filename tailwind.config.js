f974a1bfdacbdc158048a8129ab83258bfb5d986
00chestnut <maksymilian@witoszek.com>
2025-03-19 17:31:28 +0100

    Header dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
       "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust according to your project structure
       "./node_modules/flowbite/**/*.js"   // Include Flowbite components
